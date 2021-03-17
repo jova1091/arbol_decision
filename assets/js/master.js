2b6b53cd77d162a5914eba4a4fc251f514246d72
Joan Preciado <js.preciadova@gmail.com>
2021-03-17 18:33:12 -0500

    Implementación de diseño en infografía
    Script para carga de datos, estructura de datos, estructura HTML y estilos

---
+++ assets/js/master.js
@@ -0,0 +1,81 @@
+$(document).ready(() => {
+    let dataJson        = "";
+    const requestURL    = 'data.json';
+    const request       = new XMLHttpRequest();
+    
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        dataJson = request.response;
+        showScreens(dataJson, '');
+    }
+});
+
+function sendOption(data) {
+    let dataJson        = "";
+    const requestURL    = 'data.json';
+    const request       = new XMLHttpRequest();
+    
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        dataJson = request.response;
+        $.each(dataJson.pantallas,(i, item)=>{
+            if( item.id == data ) {
+                let newDataJson = item;
+                showScreens(newDataJson, data);
+            }
+        })
+    }
+}
+
+function showScreens(jsonObj, term) {
+
+    const tituloGeneralItem     = $("#txtGeneral h3");
+    const parrafoGeneralItem    = $("#txtGeneral p");
+    const headerOpcionesItem    = $("#header-options");
+    const opcionesItem          = $("#options div");
+
+    let tituloGen       = "";
+    let parrafoGen      = "";
+    let headerOpc       = "";
+    let headerOpcClass  = "";
+    let opciones        = "";
+    let opcionesData    = "";
+    
+    if(term == '' || term == null){
+        tituloGen       = jsonObj.pantallas.pantalla1.titulo;
+        parrafoGen      = jsonObj.pantallas.pantalla1.txtGeneral;
+        headerOpc       = jsonObj.pantallas.pantalla1.opcContainer.header.content;
+        headerOpcClass  = jsonObj.pantallas.pantalla1.opcContainer.header.class;
+        opciones        = jsonObj.pantallas.pantalla1.opcContainer.opciones;
+    } else {
+        tituloGen       = jsonObj.titulo;
+        parrafoGen      = jsonObj.txtGeneral;
+        headerOpc       = jsonObj.opcContainer.header.content;
+        headerOpcClass  = jsonObj.opcContainer.header.class;
+        opciones        = jsonObj.opcContainer.opciones;
+    }
+    
+    $.each(opciones,(i, item) => {
+        let opcTemplate = `
+        <div class="form-check d-flex flex-column justify-content-start col-md-6 col-sm-12 py-3">
+            <label class="form-check-label d-flex flex-column justify-content-start ${item.tipo} ${item.class}" data-option="${item.id}" for="flexRadioDefault${i}">
+                ${item.texto}
+                <img src="${item.imagen}" alt="Imagen ${item.texto}">
+                <p class="cita">${item.cita}</p>
+            </label>
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault${i}" onclick="sendOption('${item.id}')">
+        </div>
+        `;
+        opcionesData += opcTemplate;
+    })
+
+    tituloGeneralItem.html(tituloGen);
+    parrafoGeneralItem.html(parrafoGen);
+    headerOpcionesItem.attr('class', "").addClass(headerOpcClass);
+    headerOpcionesItem.html(headerOpc);
+    opcionesItem.html(opcionesData);
+}
